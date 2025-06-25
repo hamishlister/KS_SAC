f0cc558ee01a7152c2e86ce25bf98ab6f80e3fb0
Hamish Lister <s2030435@login02.ecdf.ed.ac.uk>
2025-06-25 13:09:23 +0100

    changes

--- main_sac.py
+++ main_sac.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("config", type=str, nargs="?", default="configs/config_sac.yaml")
-    parser.add_argument("--ensemble_index", type=int, default=0)
+    parser.add_argument("--ensemble_index", type=int, default=1)
     args = parser.parse_args()
 
     from SAC import main
